b4313bd7ea9db122c9b76475daf5ae6833147c6d
Taisia Makhmutova <taia.makh@gmail.com>
2021-05-29 23:44:30 +0200

    add search inputs

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,35 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import Search from './Search'
+import React, { useEffect, useState } from "react";
+import Search from "./Search";
 
 export default function List() {
-    const [ data, setData ] = useState([])
-    const [ searchName, setSearchName ] = useState('')
-    const [ searchAge, setSearchAge ] = useState('')
+  const [data, setData] = useState([]);
+  const [searchName, setSearchName] = useState("");
+  const [searchAge, setSearchAge] = useState("");
 
-    useEffect(() => {
-       fetch('https://randomuser.me/api?results=50')
-       .then(response => response.json())
-       .then((data) => {
-           setData(data.results)
-           //setFiltred(data.results)
-        })
+  useEffect(() => {
+    fetch("https://randomuser.me/api?results=50")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data.results);
+      });
+  }, []);
 
-    }, [])
+  console.log(data);
 
-    console.log(data)
-
-    return (
-        <div>
-        <p>Name</p>
-        <Search setSearchName={setSearchName}/>
-        <p>Age</p>
-        <Search setSearchAge={setSearchAge}/>
-        {data.map((el) => {
-           return (
-               <p>{el.name.title} {el.name.first}</p>
-               )
+  return (
+    <div>
+      <p>Name</p>
+      <Search setSearch={setSearchName} />
+      <p>Age</p>
+      <Search setSearch={setSearchAge} />
+      {data
+        .filter(
+          (personName) =>
+            Object.values(personName.name)
+              .join(" ")
+              .toLowerCase()
+              .includes(searchName)
+          //person.name.title.toLowerCase().includes(searchName)
+        )
+        //.filter((personAge) => personAge.dob.age == parseInt(searchAge))
+        .map((el) => {
+          return (
+            <div key={el.registered.date}>
+              <p>
+                {el.name.title} {el.name.first} {el.name.last}
+              </p>
+              <p>Age: {el.dob.age}</p>
+            </div>
+          );
         })}
-
-        </div>
-    )
+    </div>
+  );
 }
